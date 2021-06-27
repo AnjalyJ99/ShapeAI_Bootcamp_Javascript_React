d434ea3687bc43d1039c9d7260fc55be68c3bb86
AnjalyJ <68194081+AnjalyJ99@users.noreply.github.com>
2021-06-27 12:11:08 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>JavaScript and ReactJS are correlated with each other.React JS is a library based on JavaScript.</p>
+    </div>
+  );
+}
+
+export default Info;
